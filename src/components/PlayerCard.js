3a7a951c2387b9a144c4fec2921e43356c53ed41
Mark McCann <mjmccann12@me.com>
2020-01-30 15:12:16 -0600

    began player card

---
+++ src/components/PlayerCard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import { Link } from "react-router-dom";
+
+class PlayCard extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-content">
+                    <picture>
+                        <img src={require('./placeholder.svg')} alt="Player picture" />
+                    </picture>
+                    <h3>Name:<span className="card-player-name">John Smith</span></h3>
+                    <p>Position: Water boy</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PlayCard;
