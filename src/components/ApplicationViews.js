88e345148e86e9e5a465d70ff7f14cee860bab29
Mark McCann <mjmccann12@me.com>
2020-02-03 14:00:49 -0600

    added a delete method

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,7 @@ import { Route } from 'react-router-dom'
 import React, { Component } from 'react';
 import Home from './home/Home'
 import PlayerCard from './players/PlayerCard'
+import PlayerDetail from './players/PlayerDetail'
 
 
 class ApplicationViews extends Component {
@@ -11,9 +12,19 @@ class ApplicationViews extends Component {
                 <Route exact path="/" render={(props) => {
                     return <Home />
                 }} />
+
+
                 <Route exact path="/players" render={(props) => {
                     return <PlayerCard />
                 }} />
+                <Route exact path="/players" render={(props) => {
+                    return <PlayerList />
+                }} />
+                <Route exact path="/players/:playerId(\d+)" render={(props) => {
+                    return <PlayerDetail playerId={parseInt(props.match.params.playerId)} {...props} />
+                }} />
+
+
             </React.Fragment>
         )
     }
--- src/components/players/PlayerCard.js
+++ src/components/players/PlayerCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import './Player.css'
 
 class PlayCard extends Component {
     render() {
@@ -11,8 +12,10 @@ class PlayCard extends Component {
                     </picture>
                     <h3>Name:<span className="card-player-name">{this.props.player.name}</span></h3>
                     <p>Position: {this.props.player.position}</p>
+                    <button type="button" onClick={() => this.props.deletePlayer(this.props.player.id)}>Release</button>
+                    <Link to={`/players/${this.props.player.id}`}><button>Details</button></Link>
                 </div>
-            </div>
+            </div >
         );
     }
 }
