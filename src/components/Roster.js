57924deefb66852467928bc715a876cdc0a82901
Mark McCann <mjmccann12@me.com>
2020-02-06 13:11:49 -0600

    login is functioning

--- src/components/Roster.js
+++ src/components/Roster.js
@@ -4,13 +4,44 @@ import ApplicationViews from "./ApplicationViews"
 import './Roster.css';
 
 class Roster extends Component {
+    state = {
+        user: false
+    }
+    isAuthenticated = () => localStorage.getItem("credentials") !== null
+
+    setUser = (authObj) => {
+        localStorage.setItem(
+            "credentials",
+            JSON.stringify(authObj)
+        )
+        this.setState({
+            user: this.isAuthenticated()
+        });
+    }
+
+    clearUser = () => {
+        localStorage.clear()
+
+        this.setState({
+            user: this.isAuthenticated()
+        });
+    }
+    componentDidMount() {
+        this.setState({
+            user: this.isAuthenticated()
+        })
+    }
+
+
+
     render() {
         return (
             <div>
                 <h1></h1>
                 <div>
-                    <Navbar />
-                    <ApplicationViews />
+                    <Navbar user={this.state.user} clearUser={this.clearUser} />
+                    <ApplicationViews user={this.state.user}
+                        setUser={this.setUser} />
                 </div>
             </div>
         );
